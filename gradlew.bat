65128cce3dba228ffffbe51667a2ee9d93db248c
NETRIX342 <choudharyamit46533@gmail.com>
2025-12-01 18:52:32 +0530

    Refactor gradlew.bat for improved readability

--- gradlew.bat
+++ gradlew.bat
@@ -1,13 +1,9 @@
-@echo off
-setlocal
+@ECHO OFF
+SET DIRNAME=%~dp0
+IF "%DIRNAME%" == "" SET DIRNAME=.
+SET APP_BASE_NAME=%~n0
+SET APP_HOME=%DIRNAME%
 
-set DIRNAME=%~dp0
-if "%DIRNAME%" == "" set DIRNAME=.
-set CLASSPATH=%DIRNAME%gradle\wrapper\gradle-wrapper.jar
+SET CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-set JAVA_EXE=java
-if defined JAVA_HOME (
-    set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-)
-
-"%JAVA_EXE%" -cp "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
